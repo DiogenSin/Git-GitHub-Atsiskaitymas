1500156f08eb60183ce80234d61daf9eda12a9fb
DiogenSin <jakasdovydas@gmail.com>
2023-01-09 14:41:57 +0200

    Homepage show/hide function

--- scripts/main.js
+++ scripts/main.js
@@ -12,6 +12,8 @@ workBtn.addEventListener('click', e => {
     e.preventDefault()
     console.log(e)
     document.getElementById('workTab').classList.remove('hide')
+    document.querySelector('main').classList.add('hide') // paslepia homepage turinį
+    document.querySelector('nav').classList.add('hide')
 })
 
 let aboutBtn = document.getElementById('about')
@@ -19,6 +21,8 @@ aboutBtn.addEventListener('click', e => {
     e.preventDefault()
     console.log(e)
     document.getElementById('aboutTab').classList.remove('hide')
+    document.querySelector('main').classList.add('hide')
+    document.querySelector('nav').classList.add('hide')
 })
 
 let contactBtn = document.getElementById('contact')
@@ -26,12 +30,16 @@ contactBtn.addEventListener('click', e => {
     e.preventDefault()
     console.log(e)
     document.getElementById('contactTab').classList.remove('hide')
+    document.querySelector('main').classList.add('hide')
+    document.querySelector('nav').classList.add('hide')
 })
 
 let closeBtns = document.querySelectorAll('.close') // stebi visus close paspaudimus
 closeBtns.forEach(item => {
     item.addEventListener('click', e => {
         e.preventDefault()
+        document.querySelector('main').classList.remove('hide') // išslaptina homepage turinį
+        document.querySelector('nav').classList.remove('hide')
         document.querySelectorAll('.tab').forEach(tab => { // prideda tabams hide klases
             tab.classList.add('hide')
         })
